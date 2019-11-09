dffde36c6606fb4c3a4dc74f2eca71d659753eda
Pedro Ovidio <pedrovidio@gmail.com>
2019-11-09 18:20:45 -0300

    validacao login por middleware

--- src/routes.js
+++ src/routes.js
@@ -2,10 +2,16 @@ import { Router } from 'express'; // importação apenas da parte do roteamento.
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 
+import authMiddleware from './app/middlewares/auth';
+
 const routes = new Router();
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
+
+routes.use(authMiddleware);
+
+routes.put('/users', UserController.update);
 
 // module.exports = routes;
 export default routes;
