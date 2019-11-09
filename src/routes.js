67a6cd8a6370fd677a52a82abd694703a1a3f293
Pedro Ovidio <pedrovidio@gmail.com>
2019-11-09 17:30:25 -0300

    Configuracao jwt e inicio validacao user

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,11 @@
 import { Router } from 'express'; // importação apenas da parte do roteamento.
 import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
 
 const routes = new Router();
 
 routes.post('/users', UserController.store);
-
-routes.put('/users', UserController.store);
+routes.post('/sessions', SessionController.store);
 
 // module.exports = routes;
 export default routes;
